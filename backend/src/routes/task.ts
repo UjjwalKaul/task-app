ce8a9ab3a433e5400dbb215a1ec45c6431d19cca
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-10 20:32:14 +0530

    Added task middleware

--- backend/src/routes/task.ts
+++ backend/src/routes/task.ts
@@ -1,3 +1,12 @@
 import { Hono } from 'hono';
 
-export const taskRouter = new Hono();
+export const taskRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+taskRouter.use('/*', async (c, next) => {
+  next();
+});
