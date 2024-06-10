f85e7a877910fc7e1fea18d04c589df90aef3e24
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-11 02:55:23 +0530

    Added post body type inference for frontend

--- common/types.ts
+++ common/types.ts
@@ -17,3 +17,19 @@ export const signInInputs = z.object({
 
 export type SignupInput = z.infer<typeof signUpInputs>;
 export type SigninInput = z.infer<typeof signInInputs>;
+
+export const createTaskInput = z.object({
+  title: z.string(),
+  description: z.string(),
+  status: z.boolean().optional().default(false),
+  dueDate: z.date().optional(),
+});
+export const updateTaskInput = z.object({
+  id: z.number(),
+  title: z.string(),
+  description: z.string(),
+  status: z.boolean().optional().default(false),
+  dueDate: z.date().optional(),
+});
+export type CreateTaskInput = z.infer<typeof createTaskInput>;
+export type UpdateTaskInput = z.infer<typeof updateTaskInput>;
