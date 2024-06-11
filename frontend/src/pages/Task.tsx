4851068e692ef2f0682664a2849a2c8ab6b195a7
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-11 06:23:15 +0530

    Removed console logs

--- frontend/src/pages/Task.tsx
+++ frontend/src/pages/Task.tsx
@@ -9,16 +9,11 @@ export default function Task() {
   const { loading, task } = useTask(id ?? '');
 
   const handleComplete = async (taskId: number) => {
-    console.log(`Task with id ${taskId} marked as completed`);
-    // Add logic to mark the task as completed
     try {
       await deleteTask(taskId);
-      console.log(`Task with id ${taskId} deleted successfully`);
       navigate('/tasks');
-      // Optionally, you can perform some action after the task is deleted, like refreshing the task list
     } catch (error) {
-      console.error('Error deleting task:', error);
-      // Optionally, you can handle errors here, like displaying an error message to the user
+      alert('Error deleting task: ' + error);
     }
   };
 
