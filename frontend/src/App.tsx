bf5b14bfe7c75af99212dbd9c01cae5ce11b8a02
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-11 07:36:35 +0530

    Added signup as main route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Tasks from './pages/Tasks';
@@ -9,6 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signup" replace />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/tasks" element={<Tasks />} />
